b0bb22b81bf1937a488dd06dd7ae833989a79cae
Shandre Lourens <JackNytely>
2023-09-30 07:18:01 +0200

    Added Functionality to Visit the Root of the Express Server and see the displayed heart rate

--- src/app.ts
+++ src/app.ts
@@ -10,49 +10,6 @@ import { Heart_Rate, Heart_Rate_Data } from "./Heart_Rate";
 //Setup the Heart Rate Client
 const Heart_Rate_Client = new Heart_Rate();
 
-//Start Monitoring the Heart Rate
-Heart_Rate_Client.Monitor_Heart_Rate();
-
-//Setup the Heart Rate Data Path
-const Heart_Rate_Data_Path = `${process.cwd()}/Heart_Rate_Data/HRD${new Date().getTime()}.json`;
-
-//Create the Heart Rate Data File
-writeFileSync(Heart_Rate_Data_Path, "");
-
-//Open the Heart Rate Data File and get it's Descriptor (Where it is in memory to write to it)
-const Heart_Rate_Data_File_Descriptor = openSync(Heart_Rate_Data_Path, "r+");
-
-//Listen for Heart Rate Events
-Heart_Rate_Client.on("Heart_Rate_Data", (Heart_Rate_Data: Heart_Rate_Data) => {
-	//
-	//Get the Heart Rate Data File's Statistics
-	const Heart_Rate_Data_File_Stats = fstatSync(Heart_Rate_Data_File_Descriptor);
-
-	//Setup the File Offset (-1 To overwrite the Last Character in the File and using Math.max to prevent going under the minimum)
-	const Heart_Rate_Data_File_Offset = Math.max(0, Heart_Rate_Data_File_Stats.size - 1);
-
-	//Setup the Heart Rate Data String Variable (This will be modified to make it more usable)
-	let heart_rate_data_string = JSON.stringify(Heart_Rate_Data);
-
-	//Check if this file has no data and a wrap the heart rate data string into an array to initialize the first data entry
-	if (Heart_Rate_Data_File_Stats.size < 1) heart_rate_data_string = `[${heart_rate_data_string}]`;
-
-	//Check if this file already has data and a prepend a comma to separate it from the previous entries and end it with an array end symbol
-	if (Heart_Rate_Data_File_Stats.size > 0) heart_rate_data_string = `,${heart_rate_data_string}]`;
-
-	//Setup the Heart Rate Data Buffer
-	const Heart_Rate_Data_Buffer = Buffer.from(heart_rate_data_string);
-
-	//Append the Data to the Heart Rate Data File
-	writeSync(
-		Heart_Rate_Data_File_Descriptor, //Get the Current File that needs to be written to
-		Heart_Rate_Data_Buffer, // The Data to be Appended
-		0, // The Start Point for the data you want to write (eg: [0 = apple: 1], [1 = pple: 1])
-		Heart_Rate_Data_Buffer.length, // The End Point for the data you want to write (eg: [data_length = apple: 1], [data_length - 1 = apple: ])
-		Heart_Rate_Data_File_Offset // Will Determine where to start writing in the file itself
-	);
-});
-
 //Setup the Connections List
 const Connections_List: Map<string, Socket.Socket> = new Map();
 
@@ -105,3 +62,53 @@ App.get("/", (req, res) => {
 function Socket_Client_Event_Handler(Listener: string, ...Arguments: [any]) {
 	//
 }
+
+//Start Monitoring the Heart Rate
+Heart_Rate_Client.Monitor_Heart_Rate();
+
+//Setup the Heart Rate Data Path
+const Heart_Rate_Data_Path = `${process.cwd()}/Heart_Rate_Data/HRD${new Date().getTime()}.json`;
+
+//Create the Heart Rate Data File
+writeFileSync(Heart_Rate_Data_Path, "");
+
+//Open the Heart Rate Data File and get it's Descriptor (Where it is in memory to write to it)
+const Heart_Rate_Data_File_Descriptor = openSync(Heart_Rate_Data_Path, "r+");
+
+//Listen for Heart Rate Events
+Heart_Rate_Client.on("Heart_Rate_Data", (Heart_Rate_Data: Heart_Rate_Data) => {
+	//
+	//Loop through the List of Socket Connections
+	for (const Socket_Connection of Connections_List.values()) {
+		//
+		//Send the Heart Rate Data to the Socket
+		Socket_Connection.emit("Heart_Rate_Data", Heart_Rate_Data);
+	}
+
+	//Get the Heart Rate Data File's Statistics
+	const Heart_Rate_Data_File_Stats = fstatSync(Heart_Rate_Data_File_Descriptor);
+
+	//Setup the File Offset (-1 To overwrite the Last Character in the File and using Math.max to prevent going under the minimum)
+	const Heart_Rate_Data_File_Offset = Math.max(0, Heart_Rate_Data_File_Stats.size - 1);
+
+	//Setup the Heart Rate Data String Variable (This will be modified to make it more usable)
+	let heart_rate_data_string = JSON.stringify(Heart_Rate_Data);
+
+	//Check if this file has no data and a wrap the heart rate data string into an array to initialize the first data entry
+	if (Heart_Rate_Data_File_Stats.size < 1) heart_rate_data_string = `[${heart_rate_data_string}]`;
+
+	//Check if this file already has data and a prepend a comma to separate it from the previous entries and end it with an array end symbol
+	if (Heart_Rate_Data_File_Stats.size > 0) heart_rate_data_string = `,${heart_rate_data_string}]`;
+
+	//Setup the Heart Rate Data Buffer
+	const Heart_Rate_Data_Buffer = Buffer.from(heart_rate_data_string);
+
+	//Append the Data to the Heart Rate Data File
+	writeSync(
+		Heart_Rate_Data_File_Descriptor, //Get the Current File that needs to be written to
+		Heart_Rate_Data_Buffer, // The Data to be Appended
+		0, // The Start Point for the data you want to write (eg: [0 = apple: 1], [1 = pple: 1])
+		Heart_Rate_Data_Buffer.length, // The End Point for the data you want to write (eg: [data_length = apple: 1], [data_length - 1 = apple: ])
+		Heart_Rate_Data_File_Offset // Will Determine where to start writing in the file itself
+	);
+});
